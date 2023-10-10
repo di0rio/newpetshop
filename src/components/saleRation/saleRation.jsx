b3d2b5ff88a3de3c1088323a8208cac54b58a90f
di0rio <caua.diorio29@gmail.com>
2023-10-10 20:37:13 -0300

    ajustes no cabeçalho (links e titulo) imagens adicionadas no container footer adicionado saleRation pré finalizado

--- src/components/saleRation/saleRation.jsx
+++ src/components/saleRation/saleRation.jsx
@@ -1,13 +1,39 @@
-import style from "./saleRation.module.css"
+import style from "./saleRation.module.css";
+
+import racao1 from "../../assets/image 2.png";
+import racao2 from "../../assets/image 3.png";
+import racao3 from "../../assets/image 4.png";
 
 const SaleRation = () => {
   return (
     <div>
-        <div>
-            ola
-        </div>
+      <div className={style.textOne}>
+        <p>Nutrientes Caninos & Comidas</p>
+      </div>
+      <div className={style.textTwo}>
+        <p>25% DE DESCONTO EM TODOS OS PRODUTOS</p>
+      </div>
+      <div className={style.ration}>
+        <img src={racao1} alt="" />
+        <img src={racao2} alt="" />
+        <img src={racao3} alt="" />
+      </div>
+      <div className={style.decription}>
+        <h5>
+          Baba | 3KG Galinha adulta e ovo Ovo, Galinha 3 kg Alimento seco para
+          cães adultos
+        </h5>
+        <h5>
+          Riacho Canino 4 KG Galinha adulta e ovo Ovo, Galinha 3 kg Alimento
+          seco para cães adultos
+        </h5>
+        <h5>
+          Biscoitos Biscork Galinha adulta e ovo Ovo, Galinha Seca Alimento para
+          cão adulto
+        </h5>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SaleRation
+export default SaleRation;
