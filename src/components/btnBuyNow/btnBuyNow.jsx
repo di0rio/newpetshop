b58d92f9da5ec3223c0fdef9eb192b21d9967434
di0rio <caua.diorio29@gmail.com>
2023-10-27 07:56:57 -0300

    alert adicionado no botão buy now

--- src/components/btnBuyNow/btnBuyNow.jsx
+++ src/components/btnBuyNow/btnBuyNow.jsx
@@ -1,9 +1,25 @@
+import Swal from "sweetalert2";
 import style from "./btnBuyNow.module.css";
 
 const BtnBuyNow = () => {
+  const handleClick = async () => {
+
+    const result = await fetch("Comprado!");
+    
+    Swal.fire({
+      background: 'black',
+      color: 'white',
+      title:"Item comprado!",
+      text:"Obrigo pelo dinheiro, volte sempre😘✌️",
+      confirmButtonColor: 'blue',
+      icon: 'info',
+      iconColor: 'blue',
+    })
+  };
+
   return (
     <div className={style.location}>
-      <button className={style.btnBuy}>Compre agora</button>
+      <button onClick={handleClick} className={style.btnBuy}>Compre agora</button>
     </div>
   );
 };
